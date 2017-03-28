74a9598e791a4675497f047ae82b5732aefe470b
kei-g <km.8k6ce+github@gmail.com>
2017-03-28 18:52:41 +0900

    :sparkles: Here is the Entraceable module

--- lib/entraceable.rb
+++ lib/entraceable.rb
@@ -1,5 +1,25 @@
 require "entraceable/version"
 
 module Entraceable
-  # Your code goes here...
+  def entraceable(method, tag: nil, level: :debug)
+    class_eval <<-EOS if Rails.env.development?
+      alias_method :original_#{method}, :#{method}
+      def #{method}(*args)
+        Rails.logger.tagged(#{tag}) {Rails.logger.send :#{level}, %Q(#{method} is called with arguments, \#\{args\})}
+        send(:original_#{method}, *args).tap do |result|
+          Rails.logger.tagged(#{tag}) {Rails.logger.send :#{level}, %Q(#{method} returns \#\{result\})}
+        end
+      end
+    EOS
+  end
+
+  def distraceable(method)
+    class_eval <<-EOS if Rails.env.development?
+      remove_method :#{method}
+      alias_method :#{method}, :original_#{method}
+      remove_method :original_#{method}
+    EOS
+  end
 end
+
+Object.extend Entraceable
