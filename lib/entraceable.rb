71968b2ad702abfaaed4647fa0719ec21559f991
kei-g <km.8k6ce+github@gmail.com>
2017-03-29 14:09:40 +0900

    :sparkles: Support indexer methods
    [] and []= are supported by replacing brackets with "indexer"

--- lib/entraceable.rb
+++ lib/entraceable.rb
@@ -2,11 +2,12 @@ require "entraceable/version"
 
 module Entraceable
   def entraceable(method, tag: nil, level: :debug)
+    alias_name = Entraceable.alias_name_for method
     class_eval <<-EOS if Rails.env.development?
-      alias_method :original_#{method}, :#{method}
+      alias_method :#{alias_name}, :#{method}
       def #{method}(*args)
         Rails.logger.tagged(#{tag}) {Rails.logger.send :#{level}, %Q(#{method} is called with arguments, \#\{args\})}
-        send(:original_#{method}, *args).tap do |result|
+        send(:#{alias_name}, *args).tap do |result|
           Rails.logger.tagged(#{tag}) {Rails.logger.send :#{level}, %Q(#{method} returns \#\{result\})}
         end
       end
@@ -14,12 +15,18 @@ module Entraceable
   end
 
   def distraceable(method)
+    alias_name = Entraceable.alias_name_for method
     class_eval <<-EOS if Rails.env.development?
       remove_method :#{method}
-      alias_method :#{method}, :original_#{method}
-      remove_method :original_#{method}
+      alias_method :#{method}, :#{alias_name}
+      remove_method :#{alias_name}
     EOS
   end
+
+  private
+    def self.alias_name_for(method)
+      ["original", method.to_s.sub(/\[\]/, "indexer")].join("_").intern
+    end
 end
 
 Object.extend Entraceable
