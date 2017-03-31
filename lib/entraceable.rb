c120c7d633f6340b06b8f10a861080144f1df6d8
kei-g <km.8k6ce+github@gmail.com>
2017-03-31 23:46:36 +0900

    :sparkles: Add members to configure

--- lib/entraceable.rb
+++ lib/entraceable.rb
@@ -2,13 +2,37 @@ require "entraceable/version"
 require "rails"
 
 module Entraceable
+  @enabled = Rails.env.development?
+
+  class << self
+    def disable
+      @enabled = false
+    end
+
+    def enable
+      @enabled = true
+    end
+
+    def enabled?
+      @enabled
+    end
+
+    def logger
+      @logger ||= Rails.logger
+    end
+
+    def logger=(arg)
+      @logger = arg
+    end
+  end
+
   def entraceable(method, tag: nil, level: :debug)
     alias_name = alias_name_for method
-    class_eval <<-EOS if Rails.env.development?
+    class_eval <<-EOS
       alias_method :#{alias_name}, :#{method}
       def #{method}(*args)
         indent = " " * ((@indent_level ||= 0) * 2)
-        puts = ->c{Rails.logger.tagged(%Q(#{tag})) {Rails.logger.send :#{level}, indent + c}}
+        puts = ->c{Entraceable.logger.tagged(%Q(#{tag})) {Entraceable.logger.send :#{level}, indent + c} if Entraceable.enabled?}
         puts.call %Q(#{method} is called with arguments, \#\{args.map(&:inspect).join(", ")\})
         @indent_level += 1
         begin
@@ -22,7 +46,7 @@ module Entraceable
 
   def distraceable(method)
     alias_name = alias_name_for method
-    class_eval <<-EOS if Rails.env.development?
+    class_eval <<-EOS
       remove_method :#{method}
       alias_method :#{method}, :#{alias_name}
       remove_method :#{alias_name}
